b2fed4aca87e22cdc5c4a6a0f337e651bcbc0e0e
miaomiao.yang <18334757882@163.com>
2020-01-16 11:41:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: yangmiaomiao
+ * @Date: 2020-01-16 11:39:57
+ * @LastEditors: yangmiaomiao
+ * @LastEditTime: 2020-01-16 11:40:19
+ * @Description: 
+ */
+window.onload = function () {
+  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+}
